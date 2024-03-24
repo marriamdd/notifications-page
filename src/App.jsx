0e4c3e1e2fb8d1b88b4b5e437019c19a0a73f623
marriamdd <davitashvili.m3@gmail.com>
2024-03-24 15:37:56 +0400

    Set up project structure and configurations
    - Created project folders and files
    - Added necessary configuration files (e.g., package.json, .gitignore)
    - Set up basic project structure and boilerplate code

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import './App.css';
+import { GlobalStyles } from './Globals';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <GlobalStyles />
+      
+     
+    </>
+  );
+}
+
+export default App;
